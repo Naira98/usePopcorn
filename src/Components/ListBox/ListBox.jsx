46b974bfbf874525ed2425900dc2a364f5a02e43
Naira98 <nairamm99@gmail.com>
2024-06-23 15:40:13 +0300

    first-static-part

---
+++ src/Components/ListBox/ListBox.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./listBox.css";
+import Movie from "../Movie/Movie";
+
+const ListBox = ({ movies }) => {
+  const [isOpen1, setIsOpen1] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && (
+        <ul className="list">
+          {movies?.map((movie) => (
+            <Movie key={movie.imdbID} movie={movie} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ListBox;
